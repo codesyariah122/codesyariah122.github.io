77c2eef47fc5f71c57be068927e6ddbcb97c0762
codesyariah122 <pujiermanto@gmail.com>
2020-11-14 21:07:14 +0700

    edit contact form

--- assets/js/MyData.js
+++ assets/js/MyData.js
@@ -2,7 +2,13 @@ const vm = new Vue({
 	el : '#contentApp',
 	data:   {
 				title1 : obj.title,
-                titleHeader : obj.titleHeader,
+				titleHeader : obj.titleHeader,
+				blockquoteHeader: obj.blockquoteHeader,
+				imageVectorCard : obj.cardVector,
+				message: obj.author,
+				tagsParallax : obj.tagsParallax,
+				imageAuthor2 : obj.authorVector2,
+				titleHeaderContact : obj.titleHeader,
 		    }
 });
 
---
+++ assets/js/contactForm.js
@@ -0,0 +1,59 @@
+window.addEventListener("DOMContentLoaded", function() {
+  
+    // get the form elements defined in your form HTML above
+    
+    var form = document.getElementById("my-form");
+    var button = document.getElementById("my-form-button");
+    var status = document.getElementById("my-form-status");
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      button.style = "display: none ";
+      status.innerHTML = `
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Terima kasih </strong> email anda sudah kami terima.
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+      `;
+    }
+
+    function error() {
+      status.innerHTML = `
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <strong>Ooopss ! ada kesalahan saat proses pengiriman data.
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+      `;
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
