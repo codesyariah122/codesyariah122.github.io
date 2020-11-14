e735bda724f676f006ae04b26cc9a1377d582728
codesyariah122 <pujiermanto@gmail.com>
2020-11-14 12:45:30 +0700

    Update project master

--- assets/js/MyProject.js
+++ assets/js/MyProject.js
@@ -1,7 +1,27 @@
 $(document).ready(function(){
-  
+    if(location.href == projURL){
+        Swal.fire({
+            title: 'Hello Welcome',
+            html: `
+                <h3><span style="font-family:Reey Regular; color:crimson;">Codesyariah Project</span></h3>
+            `,
+            width: 600,
+            padding: '3em',
+            background: `#fff url(${imgURL}project/master-bg2.jpg)`,
+            backdrop: `
+              rgba(0,0,123,0.4)
+              url("${imgURL}project/sayhalo3.gif")
+              left top
+              no-repeat
+            `
+          });
+    }
 })
 
 function baseURL(param){
     return param;
+}
+
+function imageURL(url, param){
+    return url+param;
 }
