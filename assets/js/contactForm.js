984e65408b380d5cce937aee3da8c8491a42d7fb
codesyariah122 <pujiermanto@gmail.com>
2020-11-15 13:28:49 +0700

    update contact form

--- assets/js/contactForm.js
+++ assets/js/contactForm.js
@@ -1,7 +1,7 @@
 window.addEventListener("DOMContentLoaded", function() {
   
     // get the form elements defined in your form HTML above
-    
+    const fullName = document.getElementById('fullname').value;
     var form = document.getElementById("my-form");
     var button = document.getElementById("my-form-button");
     var status = document.getElementById("my-form-status");
@@ -13,7 +13,7 @@ window.addEventListener("DOMContentLoaded", function() {
       button.style = "display: none ";
       status.innerHTML = `
         <div class="alert alert-success alert-dismissible fade show" role="alert">
-            <strong>Terima kasih </strong> email anda sudah kami terima.
+            <strong>Terima kasih </strong> <span class="namanya"></span> email anda sudah kami terima.
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
             </button>
@@ -36,8 +36,53 @@ window.addEventListener("DOMContentLoaded", function() {
 
     form.addEventListener("submit", function(ev) {
       ev.preventDefault();
+      const fullName = document.getElementById('fullname').value;
+      const email = document.getElementById('email').value;
+      const regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
       var data = new FormData(form);
-      ajax(form.method, form.action, data, success, error);
+      if(email !== ''){
+        if(!regex.test(email)){
+            Swal.fire({
+                title: 'Maaf, Format Email Tidak Valid.',
+                width: 600,
+                padding: '3em',
+                background: `#fff url(${obj.validationBackdrop}author5.jpg)`,
+                backdrop: `
+                rgba(0,0,123,0.4)
+                url("${obj.validationGif}fails.gif")
+                left top
+                no-repeat
+                `
+            });
+        } else {
+            Swal.fire({
+              title: `Terimakasih ... ${fullName}`,
+              width: 600,
+              padding: '3em',
+              background: `#fff url(${obj.validationBackdrop}author5.jpg)`,
+              backdrop: `
+              rgba(0,0,123,0.4)
+              url("${obj.validationGif}yes.gif")
+              left top
+              no-repeat
+              `
+            });
+          ajax(form.method, form.action, data, success, error);
+        }
+      }else{
+          Swal.fire({
+            title: 'Maaf, Contact form belum di isi dengan benar.',
+            width: 600,
+            padding: '3em',
+            background: `#fff url(${obj.validationBackdrop}author5.jpg)`,
+            backdrop: `
+            rgba(0,0,123,0.4)
+            url("${obj.validationGif}empty.gif")
+            left top
+            no-repeat
+            `
+          });
+      }
     });
   });
   
