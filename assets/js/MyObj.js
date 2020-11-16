911fec0aaff71bc8a5c177830adb4b31d77a379b
codesyariah122 <pujiermanto@gmail.com>
2020-11-16 14:05:46 +0700

    Update project search movie

--- assets/js/MyObj.js
+++ assets/js/MyObj.js
@@ -1,5 +1,8 @@
 const data = {
     'MyNav' : $('#MagicMenu'),
     'NavDown' : $('.nav-down'),
-    'NavUp' : $('.nav-up')
+    'NavUp' : $('.nav-up'),
+    'searchMovie' : $('#search-input'),
+    'movieList' : $('#movie-list'),
+    'searchButton' : $('#search-button'),
 };
--- assets/js/MyProject.js
+++ assets/js/MyProject.js
@@ -5,12 +5,12 @@ $(document).ready(function(){
             html: `
                 <h3><span style="font-family:Reey Regular; color:crimson;">Codesyariah Project</span></h3>
             `,
-            width: 300,
-            padding: '3em',
+            width: 600,
+            padding: '5rem',
             background: `#fff url(${imgURL}project/master-bg2.jpg)`,
             backdrop: `
               rgba(0,0,123,0.4)
-              url("${imgURL}project/sayhalo.gif")
+              url("${imgURL}project/sayhalo1.gif")
               left top
               no-repeat
             `
