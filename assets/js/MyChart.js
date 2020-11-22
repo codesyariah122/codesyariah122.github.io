ab5658bff596c59f9681e15c589e078ecc4b1085
codesyariah122 <pujiermanto@gmail.com>
2020-11-22 17:35:07 +0700

    Update line chart js

--- assets/js/MyChart.js
+++ assets/js/MyChart.js
@@ -25,29 +25,52 @@ const covidChart = (last_date, labels, label, dataCovid) => {
     const ctx = document.getElementById('myChart').getContext('2d');
     if(window.bar != undefined) 
     window.bar.destroy(); 
+    // window.bar  = new Chart(ctx, {
+    //     type: 'bar',
+    //     data: {
+    //         labels: labels,
+    //         datasets: [{
+    //             label: `# ${label} | Update : ${last_date}`,
+    //             data: dataCovid,
+    //             backgroundColor: [
+    //                 'rgba(255, 99, 132, 0.2)',
+    //                 'rgba(54, 162, 235, 0.2)',
+    //                 'rgba(255, 206, 86, 0.2)',
+    //                 'rgba(75, 192, 192, 0.2)',
+    //                 'rgba(153, 102, 255, 0.2)',
+    //                 'rgba(255, 159, 64, 0.2)'
+    //             ],
+    //             borderColor: [
+    //                 'rgba(255, 99, 132, 1)',
+    //                 'rgba(54, 162, 235, 1)',
+    //                 'rgba(255, 206, 86, 1)',
+    //                 'rgba(75, 192, 192, 1)',
+    //                 'rgba(153, 102, 255, 1)',
+    //                 'rgba(255, 159, 64, 1)'
+    //             ],
+    //             borderWidth: 1
+    //         }]
+    //     },
+    //     options: {
+    //         scales: {
+    //             yAxes: [{
+    //                 ticks: {
+    //                     beginAtZero: true
+    //                 }
+    //             }]
+    //         }
+    //     }
+    // });
+    // myChart.update();
     window.bar  = new Chart(ctx, {
-        type: 'bar',
+        type: 'line',
         data: {
             labels: labels,
             datasets: [{
                 label: `# ${label} | Update : ${last_date}`,
                 data: dataCovid,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132,1)',
                 borderWidth: 1
             }]
         },
@@ -56,12 +79,14 @@ const covidChart = (last_date, labels, label, dataCovid) => {
                 yAxes: [{
                     ticks: {
                         beginAtZero: true
-                    }
+                    },
+                    stacked: true
                 }]
-            }
+            },
+            responsive: true,
+            maintainAspectRatio: false
         }
     });
-    // myChart.update();
 }
 
 const genderChart = (labels, dataChartGender) => {
