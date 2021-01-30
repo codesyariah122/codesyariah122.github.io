18ca2ce78d59b5bedc5aa750967e86a6870fedc9
pujiermanto <pujiermanto@gmail.com>
2021-01-30 16:11:38 +0700

    Update location.js

--- assets/js/location.js
+++ assets/js/location.js
@@ -1,5 +1,5 @@
 LookUp("json", res => {
-		console.log("Success fetch ", res)
+	console.log("Success fetch ", res)
 }, (err) => {
-		console.log("Error fetch, ", err)
+	console.log("Error fetch, ", err)
 })
