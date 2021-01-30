eb6b5940a432696e9d977903ac10b948df0a5bb2
pujiermanto <pujiermanto@gmail.com>
2021-01-30 17:17:47 +0700

    Update location.js

--- assets/js/location.js
+++ assets/js/location.js
@@ -3,3 +3,21 @@ LookUp("json", res => {
 }, (err) => {
 	console.log("Error fetch, ", err)
 })
+
+// Google map
+let map;
+function initMap() {
+    const lat = parseFloat(Cookies.get('lat'));
+    const lng = parseFloat(Cookies.get('lng'));
+    // alert(typeof lat);
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: lat, lng: lng },
+    zoom: 7,
+  });
+
+  marker = new google.maps.Marker({
+      position: new google.maps.LatLng(lat, lng),
+      map: map
+  });
+
+}
