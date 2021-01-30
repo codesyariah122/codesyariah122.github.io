90a77e90c71c64278971690b8bf89e5187cdead7
pujiermanto <pujiermanto@gmail.com>
2021-01-30 17:33:47 +0700

    Update functions.js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -41,6 +41,22 @@ const LookUp = async(data, success, err) => {
 
 }
 
+const initMap => (map) {
+    const lat = parseFloat(Cookies.get('lat'));
+    const lng = parseFloat(Cookies.get('lng'));
+    // alert(typeof lat);
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: lat, lng: lng },
+    zoom: 7,
+  });
+
+  marker = new google.maps.Marker({
+      position: new google.maps.LatLng(lat, lng),
+      map: map
+  });
+
+}
+
 // Reading news
 const NewsMedia = (url, param, apiKey, success, err) => {
 	const req = new XMLHttpRequest()
