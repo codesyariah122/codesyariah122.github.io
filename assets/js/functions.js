77b63302b281636d42892dd7d73b2d87b1236d8a
codesyariah122 <pujiermanto@gmail.com>
2021-02-06 00:49:01 +0700

    update quran

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,105 +1,46 @@
-// Ip location and geographic
-const getLocation = async(data) => {
-	let req = await fetch(`https://cors-anywhere.herokuapp.com/http://ip-api.com/json/${data}`)
-	let res = await req.json()
-
-	return res
-}
-
-const LookUp = async(data, success, err) => {
-	const req = await fetch(`https://cors-anywhere.herokuapp.com/https://api.ipify.org/?format=${data}`)
-	const res = await req.json()
-
-	const ip = res.ip
-
-	getLocation(ip).then(res => {
-		
-		console.log('Response success : ', res)
-
-		Cookies.set('code', res.countryCode, {expires: 30})
-		Cookies.set('country', res.country, {expires: 30})
-		Cookies.set('city', res.city, {expires: 30})
-		Cookies.set('lat', res.lat, {expires: 30})
-		Cookies.set('lng', res.lon, {expires: 30})
-
-		const newEl = document.createElement('div')
-		newEl.className = 'card mt-5 mb-2'
-		newEl.setAttribute('style', 'width: 25rem;')
-		newEl.innerHTML = `
-			  <ul class="list-group list-group-flush">
-			    <li class="list-group-item">Your ip address = <b> ${res.query} </b></li>
-			    <li class="list-group-item">Country = <b>${res.country}</b> <img src="https://www.countryflags.io/${res.countryCode}/shiny/64.png" class="img-responsive circle"> </li>
-			    <li class="list-group-item">Region Name = <b>${res.regionName}</b></li>
-			    <li class="list-group-item">City = <b>${res.city}</b></li>
-			  </ul>
-		`
-		document.querySelector('#your-location').appendChild(newEl)
-
-	}).catch(err => {
-		console.log(`Error results : ${err}`)
-	})
-
+const setIP = async(url) => {
+	const req = await fetch(url)
+	return req
 }
 
 
-// Reading news
-const NewsMedia = (url, param, apiKey, success, err) => {
-	const req = new XMLHttpRequest()
+const geoLocation = async(url, data) => {
+	const req = await fetch(`${url}${data}`)
+	return req
+}
 
-	req.open('GET', `${url}${param}&${apiKey}`, true)
+const getResult = (Data) => {
+	const resEl = document.createElement('div')
+	resEl.className='card mt-2 mb-2'
+	resEl.setAttribute('style', 'width: 18rem;')
+	resEl.innerHTML = `
+		<ul class="list-group list-group-flush">
+			<li class="list-group-item">Your ip address = <b> ${Data.query} </b></li>
+			<li class="list-group-item">Country = <b>${Data.country}</b> 
+				<img src="https://www.countryflags.io/${Data.countryCode}/shiny/64.png" class="img-responsive circle">
+			</li>
+			<li class="list-group-item">Region Name = <b>${Data.regionName}</b></li>
+			<li class="list-group-item">City = <b>${Data.city}</b></li>
+		</ul>
+	`
+	document.querySelector('#result').appendChild(resEl)
+}
 
-	req.onload = () => {
-		if(req.readyState === 4){
-			if(req.status === 200){
-				success(req.response)
-			}else if(req.status === 404) {
-				err()
-			}
-		}
+
+function setCookie(cname, cvalue, exdays){
+	let date = new Date()
+	date.setTime(date.getTime() + (exdays * 24 * 60 * 60 * 1000))
+	let expires = `Expires=${date.toGMTString()}`
+
+	document.cookie=`${cname}=${cvalue};${expires};`
+}
+
+function getCookie(cname) {
+	let name =`${cname}=`;
+	let ca = document.cookie.split(';');
+	for(let i=0; i<ca.length; i++) {
+		let c = ca[i].trim();
+		if (c.indexOf(name)==0) return c.substring(name.length,c.length);
 	}
-
-	req.onerror = (err) => {
-		console.log(err)
-	}
-
-	req.send()
+	return "";
 }
-
-
-
-// Modal News
-
-const ReadNews = (e) => {
-
-	document.querySelector('.modal-title').innerHTML=''
-	document.querySelector('.modal-body').innerHTML=''
-
-	NewsMedia(param.url, param.country, param.apiKey, res => {
-		const Start = JSON.parse(res)
-		const Reads = Start.articles[e]
-
-		console.log(Reads)
-
-		const ModalHeader = document.createElement('span')
-		ModalHeader.innerHTML = `
-			${Reads.source.name} <br/>
-			<small class="text-info">${Reads.publishedAt} | ${Reads.author} </small>
-		`
-		document.querySelector('.modal-title').appendChild(ModalHeader)
-
-		const ModalBody = document.createElement('div')
-		ModalBody.className = 'col-md-12 col-xs-12 mt-2'
-		ModalBody.innerHTML = `
-			<img src="${Reads.urlToImage}" class="img-fluid"/>
-			<h6>${Reads.title}</h6>
-			<p class="text-justify">
-				${Reads.content} ... <a href="${Reads.url}" class="btn btn-success">Baca dimari</a>
-			</p>
-		`
-		document.querySelector('.modal-body').appendChild(ModalBody)
-
-	}, (err) => {
-		console.log("Res Errors : ", err)
-	})
-}
-
--- assets/js/location.js
+++ assets/js/location.js
@@ -1,8 +1,56 @@
-LookUp("json", res => {
-	console.log("Success fetch ", res)
-}, (err) => {
-	console.log("Error fetch, ", err)
+const api = {
+  ip: 'https://api.ipify.org/?format=json',
+  geo: 'http://ip-api.com/json/',
+  button: document.querySelector('#lookup'),
+}
+
+setIP(api.ip)
+.then( res => res.json())
+.then(res => {
+  setCookie('ip_addr', res.ip, 1)
 })
+
+const code = getCookie('code')
+let ip = getCookie('ip_addr')
+if(code !== ''){
+  const DataLocation = {
+    ip: getCookie('ip_addr'),
+    code: getCookie('code'),
+    country: getCookie('country'),
+    lat: getCookie('lat'),
+    lng: getCookie('lng')
+  }
+
+  geoLocation(api.geo, ip)
+  .finally(()=>{
+    api.button.style.display="none"
+  })
+  .then(res => res.json())
+  .then(res => {
+    getResult(res)
+  })
+
+}else{
+  api.button.addEventListener('click', function(){
+    console.log(ip)
+    geoLocation(api.geo, ip)
+    .finally(() => {
+      setTimeout(function(){
+        api.button.style.visibility="hidden"
+      }, 1000)
+    })
+    .then(res => res.json())
+    .then( res => {
+      getResult(res)
+      setCookie('code', res.countryCode, 1)
+      setCookie('country', res.country, 1)
+      setCookie('lat', res.lat, 1)
+      setCookie('lng', res.lon, 1)
+    })
+  })
+}
+
+// geoLocation(api.geo, document.cookie('ip_addr'))
 
 // Google map
 let map=''
