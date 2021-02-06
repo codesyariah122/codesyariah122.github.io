404bae117d0202bf8b097040b32366fc8ac978d8
codesyariah122 <pujiermanto@gmail.com>
2021-02-06 09:10:04 +0700

    update location and newsmedia

--- assets/js/newsApi.js
+++ assets/js/newsApi.js
@@ -1,3 +1,23 @@
+let code = getCookie('code')
+let ip = getCookie('ip_addr')
+
+const apiNews = {
+	ip: 'https://api.ipify.org/?format=json',
+	geo: 'http://ip-api.com/json/',
+	button: document.querySelector('#lookup'),
+	loader: document.querySelector('#loader'),
+	error: document.querySelector('#error'),
+	news: {
+		url: 'https://newsapi.org/v2/top-headlines/?',
+		code: `country=${code}&`,
+		key: 'apiKey=5effd68f01ce47589b435b22ebdb06b9',
+		select: document.querySelector('#select-news'),
+		button: document.querySelector('#enter-news'),
+		card: document.querySelector('#news-list'),
+		modal: document.querySelector('.modal-body')
+	}
+}
+
 // geo location ip address
 setIP(apiNews.ip)
 .then( res => res.json())
